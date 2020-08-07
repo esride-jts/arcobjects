5370a03e0b8f0709417be135a00097cf5c342d74
Jan Tschada <j.tschada@esri.de>
2020-08-07 12:29:50 +0000

    #3 Create a simple CMake project

---
+++ src/arcpy/arcpro/ao_engine/ao_engine.cpp
@@ -0,0 +1,12 @@
+﻿// ao_engine.cpp : Defines the entry point for the application.
+//
+
+#include "ao_engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
