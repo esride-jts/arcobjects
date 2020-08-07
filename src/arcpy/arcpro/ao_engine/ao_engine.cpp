6986f5dc611f678826df52edb5a6225328abdddc
Jan Tschada <j.tschada@esri.de>
2020-08-07 15:48:40 +0000

    #3 Loading the correct ArcGIS version - Be aware of these BSTRs

--- src/arcpy/arcpro/ao_engine/ao_engine.cpp
+++ src/arcpy/arcpro/ao_engine/ao_engine.cpp
@@ -5,16 +5,38 @@
 
 #include <pybind11/pybind11.h>
 
+#include <comutil.h>
+
 using namespace std;
 
-
-
-void init()
+int init_engine(const string& version)
 {
+	::CoInitialize(NULL);
+
+	ArcGISVersionLib::IArcGISVersionPtr version_instance(__uuidof(ArcGISVersionLib::VersionManager));
+	if (!version_instance)
+	{
+		return -1;
+	}
+
+	VARIANT_BOOL version_loaded(VARIANT_FALSE);
+
+	CComBSTR bstr_version(_com_util::ConvertStringToBSTR(version.c_str()));
+	if (FAILED(version_instance->LoadVersion(ArcGISVersionLib::esriArcGISDesktop, bstr_version, &version_loaded)))
+	{
+		return -2;
+	}
+	if (VARIANT_FALSE == version_loaded)
+	{
+		return -3;
+	}
+
+	return 0;
 }
 
-void shutdown()
+void shutdown_engine()
 {
+	::CoUninitialize();
 }
 
 
@@ -22,8 +44,8 @@ void shutdown()
 PYBIND11_MODULE(ao_engine, py_module) {
 	py_module.doc() = "ArcObjects based geospatial engine.";
 
-	py_module.def("init", &init, "Initializes the geospatial engine.");
-	py_module.def("shutdown", &shutdown, "Shuts the geospatial engine down.");
+	py_module.def("init", &init_engine, "Initializes the geospatial engine.");
+	py_module.def("shutdown", &shutdown_engine, "Shuts the geospatial engine down.");
 }
 
 
--- src/arcpy/arcpro/ao_engine/ao_engine.h
+++ src/arcpy/arcpro/ao_engine/ao_engine.h
@@ -6,3 +6,9 @@
 #include <iostream>
 
 // Additional libraries
+#include <atlbase.h>
+#include <atlstr.h>
+
+// Reference to additional headers for your program
+//ArcGISVersion "libid:6FCCEDE0-179D-4D12-B586-58C88D26CA78"
+#import "\Program Files (x86)\Common Files\ArcGIS\bin\ArcGISVersion.dll" raw_interfaces_only no_implementation
