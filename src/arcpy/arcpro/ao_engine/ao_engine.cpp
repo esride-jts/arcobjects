298c906631f6df22ce6ee1f72d47d0ad785ab814
Jan Tschada <j.tschada@esri.de>
2020-08-07 14:34:56 +0000

    #3 Minimal init and shutdown methods using pybind11 and python 3.6

--- src/arcpy/arcpro/ao_engine/ao_engine.cpp
+++ src/arcpy/arcpro/ao_engine/ao_engine.cpp
@@ -3,7 +3,30 @@
 
 #include "ao_engine.h"
 
+#include <pybind11/pybind11.h>
+
 using namespace std;
+
+
+
+void init()
+{
+}
+
+void shutdown()
+{
+}
+
+
+
+PYBIND11_MODULE(ao_engine, py_module) {
+	py_module.doc() = "ArcObjects based geospatial engine.";
+
+	py_module.def("init", &init, "Initializes the geospatial engine.");
+	py_module.def("shutdown", &shutdown, "Shuts the geospatial engine down.");
+}
+
+
 
 int main()
 {
